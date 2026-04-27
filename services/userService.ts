de7947f6d2f92b5a8dd51409079b1ffe227c262f
WilliamBPFR <williamchawillferferreira@gmail.com>
2026-04-27 12:16:06 -0400

    Configuracion Axios

---
+++ services/userService.ts
@@ -0,0 +1,39 @@
+// src/services/userService.ts
+
+import { apiRequest } from "@/axiosConfig/apiRequest";
+import {
+  CreateUserRequest,
+  CreateUserResponse,
+  User,
+} from "@/types/example_types";
+
+export const userService = {
+  getUsers: async () => {
+    return apiRequest<User[]>({
+      method: "GET",
+      url: "/users",
+    });
+  },
+
+  getUserById: async (userId: number) => {
+    return apiRequest<User>({
+      method: "GET",
+      url: `/users/${userId}`,
+    });
+  },
+
+  createUser: async (data: CreateUserRequest) => {
+    return apiRequest<CreateUserResponse, CreateUserRequest>({
+      method: "POST",
+      url: "/users",
+      data,
+    });
+  },
+
+  deleteUser: async (userId: number) => {
+    return apiRequest<{ message: string }>({
+      method: "DELETE",
+      url: `/users/${userId}`,
+    });
+  },
+};
